90d2552d814fa80e35234fc23bdffaa72cc61dbe
lurkydismal <lurkydismal@duck.com>
2023-08-08 01:29:36 -0700

    Add buttons, remove end() _resolve argument

--- assets/app.js
+++ assets/app.js
@@ -3,9 +3,15 @@ async function start( _resolve ) {
 
   console.log( exampleVariable );
 
+  MenuName( "<em>" + exampleVariable + "</em>" );
+  await MenuLabel( "<del>Button1</del>", function() { return; } );
+  await MenuLabel( "<strong>Button2</strong>", function() { return; } );
+  await MenuLabel( "<mark>Button3</mark>", function() { return; } );
+  await MenuLabel( "<i>The illusion of free chouce</i>", function() { return; } );
+
   _resolve();
 }
 
-async function end( _resolve ) {
+async function end() {
   await _Say( "Ending" );
 }
