ae940922a3fc20b31e92f959840c2115d9bf45c8
lurkydismal <lurkydismal@duck.com>
2023-08-08 01:34:37 -0700

    Change _resolve() call

--- assets/app.js
+++ assets/app.js
@@ -4,12 +4,10 @@ async function start( _resolve ) {
   console.log( exampleVariable );
 
   MenuName( "<em>" + exampleVariable + "</em>" );
-  await MenuLabel( "<del>Button1</del>", function() { return; } );
-  await MenuLabel( "<strong>Button2</strong>", function() { return; } );
-  await MenuLabel( "<mark>Button3</mark>", function() { return; } );
-  await MenuLabel( "<i>The illusion of free choice</i>", function() { return; } );
-
-  _resolve();
+  await MenuLabel( "<del>Button1</del>", function() { _resolve(); } );
+  await MenuLabel( "<strong>Button2</strong>", function() { _resolve(); } );
+  await MenuLabel( "<mark>Button3</mark>", function() { _resolve(); } );
+  await MenuLabel( "<i>The illusion of free choice</i>", function() { _resolve(); } );
 }
 
 async function end() {
